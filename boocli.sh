dcfea6b790f389befca4e7b2d10f2a1d99b2d119
denybear <98181634+denybear@users.noreply.github.com>
2022-02-07 18:50:04 +0100

    Add files via upload

---
+++ boocli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 10
+export DISPLAY=:0
+export HOME=/home/pi
+export JACK_NO_AUDIO_RESERVATION=1
+jackd --realtime --realtime-priority 70 --port-max 20 --silent -d alsa --device hw:1 --nperiods 3 --rate 48000 --period 256 &
+sleep 5
+a2jmidid -ue &
+sleep 5
+/home/pi/boocli/boocli.a /home/pi/boocli/boocli.cfg
