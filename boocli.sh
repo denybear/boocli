7f67eadb5407fab2b7c304ebeaa7975882840e84
denybear <98181634+denybear@users.noreply.github.com>
2022-09-08 08:22:30 +0200

    change audio card number to name in call to jack

--- boocli.sh
+++ boocli.sh
@@ -4,7 +4,7 @@ sleep 10
 export DISPLAY=:0
 export HOME=/home/pi
 export JACK_NO_AUDIO_RESERVATION=1
-jackd --realtime --realtime-priority 70 --port-max 20 --silent -d alsa --device hw:1 --nperiods 3 --rate 48000 --period 64 &
+jackd --realtime --realtime-priority 70 --port-max 20 --silent -d alsa --device hw:io2 --nperiods 3 --rate 48000 --period 64 &
 sleep 5
 a2jmidid -ue &
 sleep 5
